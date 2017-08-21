5483c58acf5ffc16090c92ab11b1bbf3fb4d1024
lbonilla7 <lbonillav7@gmail.com>
2017-08-20 22:10:43 -0600

    building matricula uned

--- App.js
+++ App.js
@@ -1,13 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Picker,
+  Button,
+  Alert
+} from 'react-native';
 
+const Cuatrimestres = ['Primero', 'Segundo', 'Tercero'];
+const Materias = ['Sistemas Operativos', 'Arquitectura de computadores', 'Organización de las computadoras'];
+
+function onPressMatricular (){
+  Alert.alert(
+    'La información fue almacenada exitosamente!',
+    '',
+    [
+      {text: 'OK'},
+    ],
+    { cancelable: false }
+  )
+}
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      centroUniversitario: '',
+      cedula: '',
+      nombre: '',
+      residencia:'',
+      materia: '',
+      cuatrimestre: '',
+      costo: 0
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Centro universitario</Text>
+        <TextInput
+        style={styles.textInput}
+        autoFocus={true}
+        onChangeText={(text) => this.setState({centroUniversitario: text})}
+        />
+        <Text>Cédula</Text>
+        <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => this.setState({cedula: text})}
+        />
+        <Text>Nombre</Text>
+        <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => this.setState({nombre: text})}
+        />
+        <Text>Residencia</Text>
+        <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => this.setState({residencia: text})}
+        />
+        <Text>Materia</Text>
+        <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => this.setState({materia: text})}
+        />
+        <Text>Cuatrimeste</Text>
+        <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => this.setState({cuatrimestre: text})}
+        />
+        <Text>Costo</Text>
+        <TextInput
+        style={styles.textInput}
+        onChangeText={(num) => this.setState({costo: num})}
+        />
+        <Button
+        onPress={onPressMatricular}
+        title="Matricular"
+        color="#841584"
+        accessibilityLabel="Matricular materia en Centro universitario indicado."
+      />
       </View>
     );
   }
@@ -20,4 +92,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
